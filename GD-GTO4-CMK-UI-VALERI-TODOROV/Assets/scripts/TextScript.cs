3770390640600d01c101616a445a6e952b26e316
Valeri Todorov <v.todorov@student.fontys.nl>
2018-03-08 14:48:47 +0100

    hp bar
    I have HP bar that is being controlled by two buttons. The buttons add or subtract hp.

---
+++ GD-GTO4-CMK-UI-VALERI-TODOROV/Assets/scripts/TextScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextScript : MonoBehaviour {
+    public model hp;
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        this.text.text = "HP: " + this.hp.Hp.ToString() + "%";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.text.text = "HP: " + this.hp.Hp.ToString() + "%";
+    }
+}
---
+++ GD-GTO4-CMK-UI-VALERI-TODOROV/Assets/scripts/model.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class model : MonoBehaviour {
+
+    private int hp;
+    public int step;
+
+    public void Start()
+    {
+        this.hp = 100;
+    }
+    public void AddHp()
+    {
+        if(this.hp + step>100)
+        {
+            this.hp = 100;
+        }
+        else
+        {
+            this.hp = this.hp + step;
+        }
+    }
+    public void SubstractHp()
+    {
+        this.hp = this.hp - step;
+        if (this.hp - step < 0)
+        {
+            this.hp = 0;
+        }
+        else
+        {
+            this.hp = this.hp - step;
+        }
+    }
+    public int Hp
+    {
+        get { return this.hp; }
+    }
+}
