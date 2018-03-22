f64ccfed9f8e457f296a38caf146049f6b157177
Valeri Todorov <v.todorov@student.fontys.nl>
2018-03-22 08:56:12 +0100

    MultiplayerV2

--- GD-GTO4-CMK-MULTIPLAYER-VALERI-TODOROV/Assets/GridModule/UnitFactory.cs
+++ GD-GTO4-CMK-MULTIPLAYER-VALERI-TODOROV/Assets/GridModule/UnitFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class UnitFactory : MonoBehaviour
 {
@@ -8,75 +9,109 @@ public class UnitFactory : MonoBehaviour
 
 
     public List<ResourceCost> Costs;
-    
+
     // Temporary until we figure out a better way to decide where to spawn.
     private Vector2Int SpawnCoordinate;
-
-    private void GetSpawnLocation()
+    private int x = 0;
+    private int z = 0;
+    void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if (Physics.Raycast(ray, out hit, 100.0f))
+        //    {
+        //        if (hit.transform.tag == "Cell")
+        //            {
+        //            x = Convert.ToInt32(hit.point.x);
+        //            z = Convert.ToInt32(hit.point.y);
+        //            SpawnCoordinate = new Vector2Int(Convert.ToInt32(hit.point.x), Convert.ToInt32(hit.point.x));
+        //            }
+        //        Debug.Log(hit.collider.gameObject.name);
+        //    }
+        //}
 
-        if (Physics.Raycast(ray, out hit))
+
+        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //    x = Convert.ToInt32(Input.mousePosition.x);
+        //    z = Convert.ToInt32(Input.mousePosition.y);
+        //    if (hit.transform.tag == "Cell")
+        //    {
+        //        //x = Convert.ToInt32(hit.point.x);
+        //        //z = Convert.ToInt32(hit.point.z);
+        //        SpawnCoordinate = new Vector2Int(Convert.ToInt32(hit.point.x), Convert.ToInt32(hit.point.z));
+        //    }
+        //    Debug.Log(hit.collider.gameObject.name);
+        //}
+        if (Input.GetMouseButtonDown(1))
         {
-            if (hit.transform.GetComponent(typeof(HingeJoint)))
+            Ray ray;
+            RaycastHit hit;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
             {
-                Debug.Log("This is a Player");
-            }
-            else
-            {
-                Debug.Log("This isn't a Player");
+                x = Convert.ToInt32(Input.mousePosition.x);
+                z = Convert.ToInt32(Input.mousePosition.y);
+                if (hit.collider.tag == "Cell")
+                    Instantiate(Prototype, hit.point, Quaternion.identity);
             }
         }
     }
+public string XZInfo()
+{
+    string info = this.x.ToString() + "," + this.z.ToString();
+    return info;
+}
 
-    public void SpawnUnit()
+public void SpawnUnit()
+{
+    bool canAfford = true;
+    for (int i = 0; i < Costs.Count; i++)
     {
+        if (!Costs[i].CanAfford())
+        {
+            canAfford = false;
+        }
+    }
 
-        bool canAfford = true;
+    if (canAfford)
+    {
         for (int i = 0; i < Costs.Count; i++)
         {
-            if (!Costs[i].CanAfford())
-            {
-                canAfford = false;
-            }
-        }
-        
-        
-        if (canAfford)
-        {
-            for (int i = 0; i < Costs.Count; i++)
-            {
-                Costs[i].Pay();
-            }
-
-            Unit newUnit = Instantiate(Prototype);
-            Cell cell = Map.GetCell(SpawnCoordinate.x, SpawnCoordinate.y);
-        
-            newUnit.transform.SetParent(cell.transform, false);
-        }
-        else
-        {
-            Debug.Log("Not enough resources!");
+            Costs[i].Pay();
         }
 
+        //Vector3 vector = new Vector3(x, 0, z);
+        //Unit newUnit = Instantiate(Prototype);
+        GameObject newUnit = Instantiate(Prototype);
+        //GameObject newUnit = Instantiate(Prototype, vector, Quaternion.identity);
+        Cell cell = Map.GetCell(SpawnCoordinate.x, SpawnCoordinate.y);
+
+        newUnit.transform.SetParent(cell.transform, false);
     }
-
-    [System.Serializable]
-    public class ResourceCost
+    else
     {
-        public Resource Resource;
-        public int Cost;
-
-        public bool CanAfford()
-        {
-            return Resource.CanAfford(Cost);
-        }
-
-        public void Pay()
-        {
-            Resource.RemoveAmount(Cost);
-        }
-        
+        Debug.Log("Not enough resources!");
     }
+}
+
+[System.Serializable]
+public class ResourceCost
+{
+    public Resource Resource;
+    public int Cost;
+
+    public bool CanAfford()
+    {
+        return Resource.CanAfford(Cost);
+    }
+
+    public void Pay()
+    {
+        Resource.RemoveAmount(Cost);
+    }
+
+}
 }
---
+++ GD-GTO4-CMK-MULTIPLAYER-VALERI-TODOROV/Assets/testing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour {
+
+
+    Ray ray;
+    RaycastHit hit;
+    public GameObject prefab;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
+        {
+            GameObject clone = Instantiate(prefab, Vector3.Lerp(transform.position, hit.transform.position, 0.5F), hit.transform.rotation) as GameObject;
+        }
+    }
+}
